dd555159dadaf7083ffa8edf38837787d9af494f
Antonio Gámez Díaz <antgamdia@gmail.com>
2015-03-20 14:11:01 +0100

    Simple IAgreeBuilder test

--- src/test/java/test/TestLibLocal.java
+++ src/test/java/test/TestLibLocal.java
@@ -4,8 +4,11 @@
 package test;
 
 import es.us.isa.aml.AgreementManager;
-import es.us.isa.aml.model.AgreementModel;
-import es.us.isa.aml.util.Util;
+import es.us.isa.aml.model.Agreement;
+import es.us.isa.aml.model.AgreementOffer;
+import es.us.isa.aml.model.AgreementTemplate;
+import es.us.isa.aml.translators.Translator;
+import es.us.isa.aml.translators.iagree.IAgreeBuilder;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import static org.junit.Assert.assertFalse;
@@ -21,11 +24,12 @@ public class TestLibLocal {
 
     private static final Logger LOG = Logger.getLogger(TestLibLocal.class.getName());
     private static AgreementManager serv;
-    private static AgreementModel model1, model2, model3;
+    private static AgreementTemplate model1, model2, model3;
 
     @BeforeClass
     public static void init() {
-        serv = new AgreementManager(Util.loadFile("src/test/resources/config.json"));
+        //serv = new AgreementManager(Util.loadFile("src/test/resources/config.json"));
+        serv = new AgreementManager();
 
         model1 = serv.createAgreementTemplateFromFile("src/test/resources/samples/iagree-core.at");
         model2 = serv.createAgreementTemplateFromFile("src/test/resources/samples/iagree-core-dead.at");
@@ -34,7 +38,7 @@ public class TestLibLocal {
 
     @Test
     public void testIsValid() {
-        
+
 //        LOG.log(Level.INFO, "\n-----------------TEST_1-----------------");
 //        LOG.log(Level.INFO, "\n{0}", model1.isValidFullResponse().toString());
         assertTrue(model1.isValid());
@@ -55,7 +59,30 @@ public class TestLibLocal {
         assertTrue((Boolean) model3.isValidFullResponse().get("existInconsistencies"));
         assertFalse((Boolean) model3.isValidFullResponse().get("existDeadTerms"));
         assertTrue((Boolean) model3.isValidFullResponse().get("existCondInconsTerms"));
-
     }
 
+    @Test
+    public void testAgreementToString() {
+        Translator t = new Translator(new IAgreeBuilder());
+
+        AgreementTemplate at = new AgreementTemplate(model1);
+        AgreementOffer ao = model1.generateAgreement("clienteFurioso");
+        Agreement ag = new Agreement(ao);
+
+        assertTrue(at instanceof AgreementTemplate);
+        assertTrue(ao instanceof AgreementOffer);
+        assertTrue(ag instanceof Agreement);
+
+        assertTrue(t.export(at).contains("Template"));
+        assertTrue(t.export(ao).contains("Offer"));
+        assertTrue(t.export(ag).contains("Agreement"));
+
+        assertTrue(t.export(at).contains("CreationConstraints"));
+        assertFalse(t.export(ao).contains("CreationConstraints"));
+        assertFalse(t.export(ag).contains("CreationConstraints"));
+
+        LOG.log(Level.INFO, "\n{0}", t.export(at));
+        LOG.log(Level.INFO, "\n{0}", t.export(ao));
+        LOG.log(Level.INFO, "\n{0}", t.export(ag));
+    }
 }
