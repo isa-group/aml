f0cb7aa60a2e6fb11f684de5adc843eaa9b4e116
malim360 <manuarelun@hotmail.com>
2015-12-01 08:39:32 +0100

    add test for defineAt

--- src/main/java/es/us/isa/aml/model/AgreementModel.java
+++ src/main/java/es/us/isa/aml/model/AgreementModel.java
@@ -1,20 +1,21 @@
-/*******************************************************************************
- * AML is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+/**
+ * *****************************************************************************
+ * AML is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
- * AML is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * AML is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with AML. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * AML. If not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright (C) ISA, 2015
- * Licensed under GPL (https://github.com/isa-group/aml/blob/master/LICENSE.txt)
- *******************************************************************************/
+ * Copyright (C) ISA, 2015 Licensed under GPL
+ * (https://github.com/isa-group/aml/blob/master/LICENSE.txt)
+ * *****************************************************************************
+ */
 package es.us.isa.aml.model;
 
 import java.util.HashMap;
@@ -26,227 +27,277 @@ import es.us.isa.aml.model.expression.Expression;
 import es.us.isa.aml.model.expression.Var;
 import es.us.isa.aml.util.DocType;
 import es.us.isa.aml.util.OperationResponse;
+import es.us.isa.aml.util.Util;
+import java.util.List;
+import java.util.Map.Entry;
 
 /**
  * This abstract class is used to represent an agreement model. There are three
  * types of agreement models: templates, offers and agreements.
- * 
+ *
  * @author jdelafuente
  *
  */
 public abstract class AgreementModel extends AbstractModel {
 
-	protected String id;
-	protected Double version;
-	protected Context context;
-	protected AgreementTerms agreementTerms;
-	protected AgreementManager manager;
-	protected DocType docType;
+    protected String id;
+    protected Double version;
+    protected Context context;
+    protected AgreementTerms agreementTerms;
+    protected AgreementManager manager;
+    protected DocType docType;
 
-	public AgreementModel() {
-		this.id = "";
-		this.version = 0.0;
-		this.context = new Context();
-		this.agreementTerms = new AgreementTerms();
-		this.docType = DocType.TEMPLATE;
-	}
+    public AgreementModel() {
+        this.id = "";
+        this.version = 0.0;
+        this.context = new Context();
+        this.agreementTerms = new AgreementTerms();
+        this.docType = DocType.TEMPLATE;
+    }
 
-	public AgreementModel(AgreementManager agreementManager) {
-		super();
-		this.manager = agreementManager;
-	}
+    public AgreementModel(AgreementManager agreementManager) {
+        super();
+        this.manager = agreementManager;
+    }
 
-	public String getID() {
-		return this.id;
-	}
+    public String getID() {
+        return this.id;
+    }
 
-	public void setID(String id) {
-		this.id = id;
-	}
+    public void setID(String id) {
+        this.id = id;
+    }
 
-	public Double getVersion() {
-		return this.version;
-	}
+    public Double getVersion() {
+        return this.version;
+    }
 
-	public void setVersion(Double version) {
-		this.version = version;
-	}
+    public void setVersion(Double version) {
+        this.version = version;
+    }
 
-	public Context getContext() {
-		return context;
-	}
+    public Context getContext() {
+        return context;
+    }
 
-	public void setContext(Context context) {
-		this.context = context;
-	}
+    public void setContext(Context context) {
+        this.context = context;
+    }
 
-	public AgreementTerms getAgreementTerms() {
-		return this.agreementTerms;
-	}
+    public AgreementTerms getAgreementTerms() {
+        return this.agreementTerms;
+    }
 
-	public void setAgreementTerms(AgreementTerms agreementTerms) {
-		this.agreementTerms = agreementTerms;
-	}
+    public void setAgreementTerms(AgreementTerms agreementTerms) {
+        this.agreementTerms = agreementTerms;
+    }
 
-	public void setAgreementManager(AgreementManager agreementManager) {
-		this.manager = agreementManager;
-	}
+    public void setAgreementManager(AgreementManager agreementManager) {
+        this.manager = agreementManager;
+    }
 
-	public DocType getDocType() {
-		return docType;
-	}
+    public DocType getDocType() {
+        return docType;
+    }
 
-	public void setDocType(DocType docType) {
-		this.docType = docType;
-	}
+    public void setDocType(DocType docType) {
+        this.docType = docType;
+    }
 
-	// OPERATIONS - This should be syncronized with AgreementManager methods
-	public Boolean isValid() {
-		return manager.isValid(this);
-	}
+    // OPERATIONS - This should be syncronized with AgreementManager methods
+    public Boolean isValid() {
+        return manager.isValid(this);
+    }
 
-	public OperationResponse isValidFullResponse() {
-		return manager.isValidFullResponse(this);
-	}
+    public OperationResponse isValidFullResponse() {
+        return manager.isValidFullResponse(this);
+    }
 
-	// Model operations
-	public Property getProperty(String propName) {
-		for (ConfigurationProperty cp : this.getAgreementTerms().getService()
-				.getConfigurationProperties().values()) {
-			if (cp.getId().equals(propName)) {
-				return cp;
-			}
-		}
+    // Model operations
+    public Property getProperty(String propName) {
+        for (ConfigurationProperty cp : this.getAgreementTerms().getService()
+                .getConfigurationProperties().values()) {
+            if (cp.getId().equals(propName)) {
+                return cp;
+            }
+        }
 
-		for (MonitorableProperty mp : this.getAgreementTerms()
-				.getMonitorableProperties().values()) {
-			if (mp.getId().equals(propName)) {
-				return mp;
-			}
-		}
+        for (MonitorableProperty mp : this.getAgreementTerms()
+                .getMonitorableProperties().values()) {
+            if (mp.getId().equals(propName)) {
+                return mp;
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	public Map<String, Property> getProperties() {
-		Map<String, Property> props = new HashMap<String, Property>();
+    public Map<String, Property> getProperties() {
+        Map<String, Property> props = new HashMap<String, Property>();
 
-		for (ConfigurationProperty cp : this.getAgreementTerms().getService()
-				.getConfigurationProperties().values()) {
-			props.put(cp.getId(), cp);
-		}
+        for (ConfigurationProperty cp : this.getAgreementTerms().getService()
+                .getConfigurationProperties().values()) {
+            props.put(cp.getId(), cp);
+        }
 
-		for (MonitorableProperty mp : this.getAgreementTerms()
-				.getMonitorableProperties().values()) {
-			props.put(mp.getId(), mp);
-		}
+        for (MonitorableProperty mp : this.getAgreementTerms()
+                .getMonitorableProperties().values()) {
+            props.put(mp.getId(), mp);
+        }
 
-		return props;
-	}
+        return props;
+    }
 
-	public void setProperty(String propName, Object value) {
-		Property p = getProperty(propName);
-		if (value instanceof String) {
-			Expression expr = Expression.parse((String) value);
-			for (Var v : Expression.getVars(expr)) {
-				Property prop = getProperty(v.getId().toString());
-				if (prop != null) {
-					if (prop.getExpression() != null) {
-						v.setValue(prop.getExpression().calculate());
-					}
-				}
-			}
-			p.setExpression(expr);
-		} else if (value instanceof Integer || value instanceof Double) {
-			if (p.getMetric().getType().equals("integer")
-					|| p.getMetric().getType().equals("float")) {
-				p.setExpression(new Atomic(value));
-			} else {
-				throw new IllegalArgumentException("Property " + propName
-						+ " is a property of type " + p.getMetric().getType()
-						+ " but " + value.getClass().getSimpleName()
-						+ " was found.");
-			}
-		} else if (value instanceof Boolean) {
-			if (p.getMetric().getType().equals("boolean")) {
-				p.setExpression(new Atomic(value));
-			} else {
-				throw new IllegalArgumentException("Property " + propName
-						+ " is a property of type " + p.getMetric().getType()
-						+ " but " + value.getClass().getSimpleName()
-						+ " was found.");
-			}
-		} else if (value instanceof Expression) {
-			for (Var v : Expression.getVars((Expression) value)) {
-				Property prop = getProperty(v.getId().toString());
-				if (prop != null) {
-					if (prop.getExpression() != null) {
-						v.setValue(prop.getExpression().calculate());
-					}
-				}
-			}
-			p.setExpression((Expression) value);
-		}
-	}
+    public void setProperty(String propName, Object value) {
+        Property p = getProperty(propName);
+        if (value instanceof String) {
+            Expression expr = Expression.parse((String) value);
+            for (Var v : Expression.getVars(expr)) {
+                Property prop = getProperty(v.getId().toString());
+                if (prop != null) {
+                    if (prop.getExpression() != null) {
+                        v.setValue(prop.getExpression().calculate());
+                    }
+                }
+            }
+            p.setExpression(expr);
+        } else if (value instanceof Integer || value instanceof Double) {
+            if (p.getMetric().getType().equals("integer")
+                    || p.getMetric().getType().equals("float")) {
+                p.setExpression(new Atomic(value));
+            } else {
+                throw new IllegalArgumentException("Property " + propName
+                        + " is a property of type " + p.getMetric().getType()
+                        + " but " + value.getClass().getSimpleName()
+                        + " was found.");
+            }
+        } else if (value instanceof Boolean) {
+            if (p.getMetric().getType().equals("boolean")) {
+                p.setExpression(new Atomic(value));
+            } else {
+                throw new IllegalArgumentException("Property " + propName
+                        + " is a property of type " + p.getMetric().getType()
+                        + " but " + value.getClass().getSimpleName()
+                        + " was found.");
+            }
+        } else if (value instanceof Expression) {
+            for (Var v : Expression.getVars((Expression) value)) {
+                Property prop = getProperty(v.getId().toString());
+                if (prop != null) {
+                    if (prop.getExpression() != null) {
+                        v.setValue(prop.getExpression().calculate());
+                    }
+                }
+            }
+            p.setExpression((Expression) value);
+        }
+    }
 
-	public GuaranteeTerm getGuaranteeTerm(String gtName) {
-		GuaranteeTerm gt = null;
-		for (GuaranteeTerm g : this.getAgreementTerms().getGuaranteeTerms()
-				.values()) {
-			if (g.getId().equals(gtName)) {
-				gt = g;
-			}
-		}
-		return gt;
-	}
+    public GuaranteeTerm getGuaranteeTerm(String gtName) {
+        GuaranteeTerm gt = null;
+        for (GuaranteeTerm g : this.getAgreementTerms().getGuaranteeTerms()
+                .values()) {
+            if (g.getId().equals(gtName)) {
+                gt = g;
+            }
+        }
+        return gt;
+    }
 
-	public Boolean evaluateGT(String gtName) {
-		GuaranteeTerm gt = getGuaranteeTerm(gtName);
-		if (gt != null) {
-			Expression expr = gt.getSlo().getExpression();
-			for (Var v : Expression.getVars(expr)) {
-				Property p = getProperty(v.getId().toString());
-				if (p != null) {
-					v.setValue(p.getExpression().calculate());
-				}
-			}
-			return (Boolean) expr.calculate();
-		} else {
-			throw new NullPointerException("Guarantee term " + gtName
-					+ " not found. " + "Are you sure you have declared it?");
-		}
-	}
+    public Boolean evaluateGT(String gtName) {
+        GuaranteeTerm gt = getGuaranteeTerm(gtName);
+        if (gt != null) {
+            Expression expr = gt.getSlo().getExpression();
+            for (Var v : Expression.getVars(expr)) {
+                Property p = getProperty(v.getId().toString());
+                if (p != null) {
+                    v.setValue(p.getExpression().calculate());
+                }
+            }
+            return (Boolean) expr.calculate();
+        } else {
+            throw new NullPointerException("Guarantee term " + gtName
+                    + " not found. " + "Are you sure you have declared it?");
+        }
+    }
 
-	@Override
-	public AgreementModel clone() {
-		AgreementModel model = null;
+    public Boolean evaluateGT(String gtName, List<Property> properties) {
+        GuaranteeTerm gt = getGuaranteeTerm(gtName);
+        if (gt != null) {
+            //Es necesario clonar
+            Expression expr = gt.clone().getSlo().getExpression();
+            for (Var v : Expression.getVars(expr)) {
+                Property p = getProperty(v.getId().toString());
+                if (p != null) {
+                    if (Util.containsIdProperty(properties, p)) {
+                        v.setValue(Util.getSameIdProperty(properties, p));
+                    } else {
+                        throw new NullPointerException("Property " + p
+                                + " not found. " + "Are you sure you have declared it?");
+                    }
+                }
+            }
+            return (Boolean) expr.calculate();
+        } else {
+            throw new NullPointerException("Guarantee term " + gtName
+                    + " not found. " + "Are you sure you have declared it?");
+        }
+    }
 
-		if (docType == DocType.TEMPLATE) {
-			model = new AgreementTemplate();
-		} else if (docType == DocType.OFFER) {
-			model = new AgreementOffer();
-		} else if (docType == DocType.AGREEMENT) {
-			model = new Agreement();
-		}
+    public Boolean evaluateGTByValues(String gtName, List<Entry<String, Number>> properties) {
+        GuaranteeTerm gt = getGuaranteeTerm(gtName);
+        if (gt != null) {
+            //Es necesario clonar
+            Expression expr = gt.clone().getSlo().getExpression();
+            for (Var v : Expression.getVars(expr)) {
+                Property p = getProperty(v.getId().toString());
+                if (p != null) {
+                    if (Util.containsIdPropertyByValues(properties, p)) {
+                        v.setValue(Util.getSameIdPropertyByValues(properties, p));
+                    } else {
+                        throw new NullPointerException("Property " + p
+                                + " not found. " + "Are you sure you have declared it?");
+                    }
+                }
+            }
+            return (Boolean) expr.calculate();
+        } else {
+            throw new NullPointerException("Guarantee term " + gtName
+                    + " not found. " + "Are you sure you have declared it?");
+        }
+    }
 
-		model.setID(id);
-		model.setVersion(version);
-		model.setContext(context.clone());
-		model.setAgreementTerms(agreementTerms.clone());
-		model.setAgreementManager(manager);
-		model.setDocType(docType);
+    @Override
+    public AgreementModel clone() {
+        AgreementModel model = null;
 
-		return model;
-	}
+        if (docType == DocType.TEMPLATE) {
+            model = new AgreementTemplate();
+        } else if (docType == DocType.OFFER) {
+            model = new AgreementOffer();
+        } else if (docType == DocType.AGREEMENT) {
+            model = new Agreement();
+        }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (obj instanceof AgreementModel) {
-			AgreementModel model = (AgreementModel) obj;
-			return this.getID().equals(model.getID())
-					&& this.getVersion().equals(model.getVersion());
-		} else
-			return false;
-	}
+        model.setID(id);
+        model.setVersion(version);
+        model.setContext(context.clone());
+        model.setAgreementTerms(agreementTerms.clone());
+        model.setAgreementManager(manager);
+        model.setDocType(docType);
+
+        return model;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof AgreementModel) {
+            AgreementModel model = (AgreementModel) obj;
+            return this.getID().equals(model.getID())
+                    && this.getVersion().equals(model.getVersion());
+        } else {
+            return false;
+        }
+    }
 
 }
--- src/test/java/es/us/isa/aml/test/core/TestPPI.java
+++ src/test/java/es/us/isa/aml/test/core/TestPPI.java
@@ -10,6 +10,10 @@ import es.us.isa.aml.model.AgreementModel;
 import es.us.isa.aml.model.AgreementTemplate;
 import es.us.isa.aml.translator.Translator;
 import es.us.isa.aml.translator.builders.iagree.IAgreeBuilder;
+import java.util.AbstractMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -33,8 +37,19 @@ public class TestPPI {
     @Test
     public void testCreation1() {
         AgreementManager m = new AgreementManager();
-        Translator t= new Translator(new IAgreeBuilder());
+        Translator t = new Translator(new IAgreeBuilder());
         System.out.println(t.translate(model));
         Assert.assertNotNull(model.getProperty("Time2").getDefinitionReference());
+
+        List<Entry<String, Number>> listaA = new LinkedList();
+        Entry<String, Number> par = new AbstractMap.SimpleEntry<String, Number>("Requests", 9);
+        listaA.add(par);
+        Assert.assertEquals(model.evaluateGTByValues("RequestTerm", listaA), true);
+
+        List<Entry<String, Number>> listaB = new LinkedList();
+        Entry<String, Number> par2 = new AbstractMap.SimpleEntry<String, Number>("Requests", 11);
+        listaB.add(par2);
+        Assert.assertEquals(model.evaluateGTByValues("RequestTerm", listaB), false);
+
     }
 }
