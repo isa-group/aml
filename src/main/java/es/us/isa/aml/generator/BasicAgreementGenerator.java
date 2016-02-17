230fbbdba413514ab04838bb51e64c9c55e8f134
Antonio Gámez Díaz <antgamdia@gmail.com>
2016-02-17 11:53:05 +0100

    Fixes #70

--- src/main/java/es/us/isa/aml/generator/BasicAgreementGenerator.java
+++ src/main/java/es/us/isa/aml/generator/BasicAgreementGenerator.java
@@ -1,4 +1,5 @@
-/*******************************************************************************
+/**
+ * *****************************************************************************
  * AML is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -12,9 +13,10 @@
  * You should have received a copy of the GNU General Public License
  * along with AML. If not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright (C) ISA Research Group - University of Sevilla, 2015
- * Licensed under GPL (https://github.com/isa-group/aml/blob/master/LICENSE.txt)
- *******************************************************************************/
+ * Copyright (C) ISA Research Group - University of Sevilla, 2015 Licensed under
+ * GPL (https://github.com/isa-group/aml/blob/master/LICENSE.txt)
+ * *****************************************************************************
+ */
 package es.us.isa.aml.generator;
 
 import es.us.isa.aml.model.Actor;
@@ -23,6 +25,7 @@ import es.us.isa.aml.model.AgreementOffer;
 import es.us.isa.aml.model.AgreementTemplate;
 import es.us.isa.aml.model.Role;
 import es.us.isa.aml.model.RoleType;
+import es.us.isa.aml.operations.core.csp.AreCompliant;
 import es.us.isa.aml.util.DocType;
 
 /**
@@ -31,34 +34,47 @@ import es.us.isa.aml.util.DocType;
  */
 public class BasicAgreementGenerator extends AgreementGenerator {
 
-	@Override
-	public AgreementOffer generateAgreementOfferFromTemplate(
-			String consumerName, AgreementTemplate template) {
-		AgreementOffer ao = new AgreementOffer();
-		ao.setDocType(DocType.OFFER);
-		ao.setID(template.getID() + "_" + consumerName);
-		ao.setVersion(template.getVersion());
-		ao.setContext(template.getContext().clone());
-		ao.getContext().setTemplateId(template.getID());
-		ao.getContext().setTemplateVersion(template.getVersion());
-		ao.getContext().setInitiator(
-				new Actor(consumerName, Role.Consumer, RoleType.Initiator));
-		ao.setAgreementTerms(template.getAgreementTerms().clone());
-		return ao;
-	}
+    @Override
+    public AgreementOffer generateAgreementOfferFromTemplate(String consumerName, AgreementTemplate template) {
+        AgreementOffer ao = new AgreementOffer();
+        ao.setDocType(DocType.OFFER);
+        ao.setID(template.getID() + "_" + consumerName);
+        ao.setVersion(template.getVersion());
+        ao.setContext(template.getContext().clone());
+        ao.getContext().setTemplateId(template.getID());
+        ao.getContext().setTemplateVersion(template.getVersion());
+        ao.getContext().setInitiator(new Actor(consumerName, Role.Consumer, RoleType.Initiator));
+        ao.setAgreementTerms(template.getAgreementTerms().clone());
+        return ao;
+    }
 
-	@Override
-	public Agreement generateAgreementFromAgreementOffer(String consumerName,
-			AgreementOffer offer) {
-		Agreement ag = new Agreement();
-		ag.setDocType(DocType.AGREEMENT);
-		ag.setID(offer.getID() + "_ag");
-		ag.setVersion(offer.getVersion());
-		ag.setContext(offer.getContext().clone());
-		ag.getContext().setInitiator(
-				new Actor(consumerName, Role.Consumer, RoleType.Initiator));
-		ag.setAgreementTerms(offer.getAgreementTerms().clone());
-		return ag;
-	}
+    @Override
+    public Agreement generateAgreementFromAgreementOffer(String consumerName, AgreementOffer offer) {
+        Agreement ag = new Agreement();
+        ag.setDocType(DocType.AGREEMENT);
+        ag.setID(offer.getID() + "_" + consumerName + "_ag");
+        ag.setVersion(offer.getVersion());
+        ag.setContext(offer.getContext().clone());
+        ag.getContext().setInitiator(new Actor(consumerName, Role.Consumer, RoleType.Initiator));
+        ag.setAgreementTerms(offer.getAgreementTerms().clone());
+        return ag;
+    }
+
+    public Agreement generateAgreementFromCompliantAgreementOffer(String consumerName, AgreementOffer offer, AgreementTemplate template) {
+        Agreement ag = new Agreement();
+        AreCompliant op = new AreCompliant();
+        op.analyze(template, offer);
+        if ((Boolean) op.getResult().get("compliant")) {
+            ag.setDocType(DocType.AGREEMENT);
+            ag.setID(offer.getID() + "_" + consumerName + "_ag");
+            ag.setVersion(offer.getVersion());
+            ag.setContext(offer.getContext().clone());
+            ag.getContext().setInitiator(new Actor(consumerName, Role.Consumer, RoleType.Initiator));
+            ag.setAgreementTerms(offer.getAgreementTerms().clone());
+            return ag;
+        } else {
+            throw new IllegalArgumentException("AgreementOffer must be compliant with the AgreementTemplated passed");
+        }
+    }
 
 }
--- src/test/java/es/us/isa/aml/test/core/TestStore.java
+++ src/test/java/es/us/isa/aml/test/core/TestStore.java
@@ -1,4 +1,5 @@
-/*******************************************************************************
+/**
+ * *****************************************************************************
  * AML is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -12,18 +13,22 @@
  * You should have received a copy of the GNU General Public License
  * along with AML. If not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright (C) ISA Research Group - University of Sevilla, 2015
- * Licensed under GPL (https://github.com/isa-group/aml/blob/master/LICENSE.txt)
- *******************************************************************************/
+ * Copyright (C) ISA Research Group - University of Sevilla, 2015 Licensed under
+ * GPL (https://github.com/isa-group/aml/blob/master/LICENSE.txt)
+ * *****************************************************************************
+ */
 package es.us.isa.aml.test.core;
 
 import es.us.isa.aml.AgreementManager;
 import es.us.isa.aml.Store;
+import es.us.isa.aml.generator.BasicAgreementGenerator;
+import es.us.isa.aml.model.Agreement;
 import es.us.isa.aml.model.AgreementOffer;
 import es.us.isa.aml.model.AgreementTemplate;
 import es.us.isa.aml.util.Util;
-import org.junit.Assert;
+import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * @author AntonioGamez
@@ -31,115 +36,129 @@ import org.junit.Test;
  */
 public class TestStore {
 
-	@Test
-	public void testCreation1() {
-		AgreementManager m = new AgreementManager();
+    @Test
+    public void testCreation1() {
+        AgreementManager m = new AgreementManager();
 
-		// MODO_1:
-		String path1 = "src/test/resources/core-pack/iagree-core.at";
-		AgreementTemplate template1 = m.createAgreementTemplateFromFile(path1);
-		Assert.assertNotNull(template1);
-	}
+        // MODE_1:
+        String path1 = "src/test/resources/core-pack/iagree-core.at";
+        AgreementTemplate template1 = m.createAgreementTemplateFromFile(path1);
+        assertNotNull(template1);
+    }
 
-	@Test
-	public void testCreation2() {
-		AgreementManager m = new AgreementManager();
+    @Test
+    public void testCreation2() {
+        AgreementManager m = new AgreementManager();
 
-		// MODO_2:
-		String path2 = "src/test/resources/core-pack/iagree-core.at";
-		String content2 = Util.loadFile(path2);
-		AgreementTemplate template2 = m.createAgreementTemplate(content2);
-		Assert.assertNotNull(template2);
-	}
+        // MODE_2:
+        String path2 = "src/test/resources/core-pack/iagree-core.at";
+        String content2 = Util.loadFile(path2);
+        AgreementTemplate template2 = m.createAgreementTemplate(content2);
+        assertNotNull(template2);
+    }
 
-	@Test
-	public void testRegistration1() {
-		AgreementManager m = new AgreementManager();
+    @Test
+    public void testRegistration1() {
+        AgreementManager m = new AgreementManager();
 
-		// MODO_1:
-		String path1 = "src/test/resources/core-pack/iagree-core.at";
-		AgreementTemplate template1 = m.createAgreementTemplateFromFile(path1);
-		m.registerTemplate(template1);
-		AgreementTemplate template1R = m.getAgreementTemplate();
-		Assert.assertNotNull(template1R);
-	}
+        // MODE_1:
+        String path1 = "src/test/resources/core-pack/iagree-core.at";
+        AgreementTemplate template1 = m.createAgreementTemplateFromFile(path1);
+        m.registerTemplate(template1);
+        AgreementTemplate template1R = m.getAgreementTemplate();
+        assertNotNull(template1R);
+    }
 
-	@Test
-	public void testRegistration2a() {
-		AgreementManager m = new AgreementManager();
-		Store s = m.getStoreManager();
+    @Test
+    public void testRegistration2a() {
+        AgreementManager m = new AgreementManager();
+        Store s = m.getStoreManager();
 
-		// MODO_2a:
-		String path2a = "src/test/resources/core-pack/iagree-core.at";
-		AgreementTemplate template2a = m
-				.createAgreementTemplateFromFile(path2a);
-		s.register(template2a);
-		AgreementTemplate template2aR = s.getAgreementTemplate(template2a
-				.getID());
-		Assert.assertNotNull(template2aR);
-	}
+        // MODE_2a:
+        String path2a = "src/test/resources/core-pack/iagree-core.at";
+        AgreementTemplate template2a = m.createAgreementTemplateFromFile(path2a);
+        s.register(template2a);
+        AgreementTemplate template2aR = s.getAgreementTemplate(template2a.getID());
+        assertNotNull(template2aR);
+    }
 
-	@Test
-	public void testRegistration2b() {
-		AgreementManager m = new AgreementManager();
-		Store s = m.getStoreManager();
+    @Test
+    public void testRegistration2b() {
+        AgreementManager m = new AgreementManager();
+        Store s = m.getStoreManager();
 
-		// MODO_2b:
-		String path2b = "src/test/resources/core-pack/iagree-core.at";
-		AgreementTemplate template2b = m
-				.createAgreementTemplateFromFile(path2b);
-		s.register("hola", template2b);
-		AgreementTemplate template2bR = s.getAgreementTemplate("hola");
-		Assert.assertNotNull(template2bR);
-	}
+        // MODE_2b:
+        String path2b = "src/test/resources/core-pack/iagree-core.at";
+        AgreementTemplate template2b = m.createAgreementTemplateFromFile(path2b);
+        s.register("test", template2b);
+        AgreementTemplate template2bR = s.getAgreementTemplate("test");
+        assertNotNull(template2bR);
+    }
 
-	@Test
-	public void testRegistration3a() {
-		AgreementManager m = new AgreementManager();
-		Store s = m.getStoreManager();
+    @Test
+    public void testRegistration3a() {
+        AgreementManager m = new AgreementManager();
+        Store s = m.getStoreManager();
 
-		// MODO_3a
-		String path3a = "src/test/resources/core-pack/iagree-core.at";
-		AgreementTemplate template3a = m
-				.createAgreementTemplateFromFile(path3a);
-		s.register(template3a);
-		AgreementTemplate template3aR = s.getAgreementTemplate(template3a
-				.getID());
-		Assert.assertNotNull(template3aR);
-	}
+        // MODE_3a
+        String path3a = "src/test/resources/core-pack/iagree-core.at";
+        AgreementTemplate template3a = m.createAgreementTemplateFromFile(path3a);
+        s.register(template3a);
+        AgreementTemplate template3aR = s.getAgreementTemplate(template3a.getID());
+        assertNotNull(template3aR);
+    }
 
-	@Test
-	public void testRegistration3b() {
-		AgreementManager m = new AgreementManager();
-		Store s = m.getStoreManager();
+    @Test
+    public void testRegistration3b() {
+        AgreementManager m = new AgreementManager();
+        Store s = m.getStoreManager();
 
-		// MODO_3b
-		String path3b = "src/test/resources/core-pack/iagree-core.at";
-		AgreementTemplate template3b = m
-				.createAgreementTemplateFromFile(path3b);
-		s.register("hola", template3b);
-		AgreementTemplate template3bR = s.getAgreementTemplate("hola");
-		Assert.assertNotNull(template3bR);
-	}
+        // MODE_3b
+        String path3b = "src/test/resources/core-pack/iagree-core.at";
+        AgreementTemplate template3b = m.createAgreementTemplateFromFile(path3b);
+        s.register("test", template3b);
+        AgreementTemplate template3bR = s.getAgreementTemplate("test");
+        assertNotNull(template3bR);
+    }
 
-	@Test
-	public void testAll() {
-		AgreementManager m = new AgreementManager();
-		Store s = m.getStoreManager();
+    @Test
+    public void testAll() {
+        AgreementManager m = new AgreementManager();
+        Store s = m.getStoreManager();
 
-		String path = "src/test/resources/core-pack/iagree-core.at";
-		AgreementTemplate template = m.createAgreementTemplateFromFile(path);
+        String path = "src/test/resources/core-pack/iagree-core.at";
+        AgreementTemplate template = m.createAgreementTemplateFromFile(path);
 
-		s.register("plantilla", template);
+        s.register("plantilla", template);
+        s.register("oferta", s.getAgreementTemplate("plantilla").generateAgreementOffer("cliente"));
 
-		s.register("oferta", s.getAgreementTemplate("plantilla")
-				.generateAgreementOffer("cliente"));
+        AgreementOffer offer = s.getAgreementOffer("oferta");
 
-		AgreementOffer offer = s.getAgreementOffer("oferta");
+        assertNotNull(template);
+        assertNotNull(offer);
 
-		Assert.assertNotNull(template);
-		Assert.assertNotNull(offer);
+    }
 
-	}
+    @Test
+    public void generateAgreementFromCompliantOffer() {
+        AgreementManager m = new AgreementManager();
+        Store s = m.getStoreManager();
+
+        String pathT = "src/test/resources/core-pack/governify-multiplan.at";
+        AgreementTemplate template = m.createAgreementTemplateFromFile(pathT);
+
+        String pathO = "src/test/resources/core-pack/governify-proPlan.ao";
+        AgreementOffer offer = m.createAgreementOfferFromFile(pathO);
+
+        s.register("template", template);
+        s.register("offer", offer);
+
+        Agreement agreement = new BasicAgreementGenerator().generateAgreementFromCompliantAgreementOffer("client", offer, template);
+
+        assertNotNull(template);
+        assertNotNull(offer);
+        assertNotNull(agreement);
+
+    }
 
 }
